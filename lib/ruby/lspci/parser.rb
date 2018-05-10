fbd85f0c4db3c30f26a45c69c57c167c37d67d5f
Hebron George <hebrontgeorge@gmail.com>
2018-05-09 21:21:52 -0500

    got the parser working for 'lspci -vmm' on lspci version 3.3.1

--- lib/ruby/lspci/parser.rb
+++ lib/ruby/lspci/parser.rb
@@ -1,21 +1,51 @@
+require 'active_support/core_ext/hash/indifferent_access'
+
 module Ruby::Lspci
   class Parser
     attr_reader :command_result
 
-    def self.parse(command_result)
+    def self.parse(command_result=nil)
       new(command_result).parse
     end
 
-    def initialize(command_result)
+    def initialize(command_result=nil)
       @command_result = command_result
     end
 
-    def lspci_version
-      @version ||= `lspci --version`
+    def lspci_output
+      @lspci_output ||= `lspci -vmm`
     end
 
     def parse
+      devices = []
+      command_result = (command_result || lspci_output)
 
+      split_devices  = split_devices(command_result)
+      split_devices.each do |device|
+        devices << parse_device(device)
+      end
+
+      devices
+    end
+
+    private
+
+    def split_devices(command_result)
+      delimiter = "\n\n"
+      command_result.split(delimiter)
+    end
+
+    def parse_device(device_string)
+      device = HashWithIndifferentAccess.new
+      delimiter = ":\t"
+      details = device_string.split("\n")
+
+      details.each do |line|
+        split_details = line.split(delimiter)
+        device[split_details.shift.downcase] = split_details.map(&:strip).join(' ').strip
+      end
+
+      device
     end
   end
 end
